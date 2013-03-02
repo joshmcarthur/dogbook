3c782209b8e685850cd0958bf4e70afd5cfb01c9
Josh McArthur <joshua.mcarthur@gmail.com>
2013-03-03 11:00:30 +1300

    fix validation method name

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,8 +2,8 @@ class Dog < ActiveRecord::Base
   has_many :friendships
   has_many :friends, :through => :friendships
 
-  validate :name, :presence => true
-  validate :breed_id, :presence => true
+  validates :name, :presence => true
+  validates :breed_id, :presence => true
 
   belongs_to :breed
 end
